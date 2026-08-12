62b044e2ac985c4c5ca7a2b90093ec425b64b372
Mohd Zulkarnain <mohdzulkarnainrosli98@gmail.com>
2026-08-12 17:19:52 +0800

    Finished ex02, use MembershipCount to count key id

---
+++ ex02/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+std::set<int> ids;
+
+void MembershipCount(int id)
+{
+
+    if (ids.count(id))
+    {
+        std::cout << "is " << id << " Spawned? Yes \n";
+    }
+    else std::cout << "is " << id << " Spawned? No \n";
+}
+
+int main(){
+
+        ids.insert(7);
+        ids.insert(2);
+        ids.insert(7);
+        ids.insert(42);
+        ids.insert(13);
+        ids.insert(2);
+        ids.insert(42);    
+    
+        std::cout << "--spawned (unique, sorted)-- \n" ;
+    for (int id : ids) std::cout << id << '\n';
+
+    std::cout << "Count: " << ids.size() << " \n";
+
+    MembershipCount(42);
+    MembershipCount(99);
+
+}
